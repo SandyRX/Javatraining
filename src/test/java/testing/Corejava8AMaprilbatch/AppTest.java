4e6dd591ebca4f26846eaac326c7da8110560992
SandyRX <santhosh.sm1109@gmail.com>
2024-04-27 14:15:07 +0530

    new

---
+++ src/test/java/testing/Corejava8AMaprilbatch/AppTest.java
@@ -0,0 +1,20 @@
+package testing.Corejava8AMaprilbatch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
